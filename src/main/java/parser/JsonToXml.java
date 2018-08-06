0780600f87d436772bca24dcc41f7f6cb93331cd
valerius <valeriya.shvetcova@phystech.edu>
2018-08-07 02:20:05 +0300

    recursive doesn't work

--- src/main/java/parser/JsonToXml.java
+++ src/main/java/parser/JsonToXml.java
@@ -2,11 +2,20 @@ package parser;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Stack;
+
 public class JsonToXml {
     // settings
     final private String attrPrefix;
     final private String propertyName;
     final private String prefixType;
+    final private String xmlInsertName;
+
+    private Stack<String> path_field;
+    private Stack<JsonNode> path;
+    private String input;
 
     public JsonToXml(String attrPrefix, String propertyName, String prefixType) {
         this.attrPrefix = attrPrefix;
@@ -15,10 +24,27 @@ public class JsonToXml {
             prefixType = prefixType.concat(":");
         }
         this.prefixType = prefixType;
+        if (propertyName.equals("xmlInsertName")) {
+            xmlInsertName = "xmlInsertName#";
+        } else {
+            xmlInsertName = "xmlInsertName";
+        }
     }
 
     public String parse (JsonNode input) {
-        String output = new String();
-        return output;
+        return xmlRepresent(input);
+    }
+    public String xmlRepresent(JsonNode json) {
+        String out = new String();
+        for (Iterator<Map.Entry<String,JsonNode>> it = json.fields(); it.hasNext(); ) {
+            Map.Entry<String,JsonNode> field = it.next();
+            out = out.concat("<").concat(field.getKey()).concat(">").concat(xmlRepresent(field.getValue()))
+                    .concat("</").concat(field.getKey()).concat(">");
+        }
+        if (!out.isEmpty()) {
+            return out;
+        } else {
+            return json.asText();
+        }
     }
 }
